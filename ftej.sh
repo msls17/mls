7c5fd66918ffb135bfd0e515618d5d8efe44491d
msls17 <33090354+msls17@users.noreply.github.com>
2024-03-25 20:09:28 +0800

    Add files via upload

---
+++ ftej.sh
@@ -0,0 +1,31 @@
+# new Env('福田e家');
+#by-莫老师，版本1.0
+#青龙创建变量ftej，值为账号@密码，100积分等于1元，平均一天约100分
+#cron:10 1 * * *
+zh=($(echo $ftej | sed 's/&/ /g'))
+token=ebf76685e48d4e14a9de6fccc76483e3
+apptoken=58891364f56afa1b6b7dae3e4bbbdfbfde9ef489
+url=czyl.foton.com.cn
+for s in $(seq 0 1 $((${#zh[@]}-1)))
+do
+phone=$(echo ${zh[$s]} | awk -F "@" '{print $1}')
+pass=$(echo ${zh[$s]} | awk -F "@" '{print $2}')
+tmp=$(curl -sk -X POST -H "Content-Type: application/json;charset=utf-8" -H "Host: $url" -d '{"password":"'$pass'","version_name":"7.3.4","version_auth":"l8szUT88Wl9U90WxUw/exQ\u003d\u003d","device_id":"isAppMarket","device_model":"Redmi23049RAD8C","ip":"","name":"'$phone'","version_code":"196","deviceSystemVersion":"13","device_type":"0"}' "https://$url/ehomes-new/homeManager/getLoginMember")
+code=$(echo $tmp | jq -r '.data.memberComplexCode')
+uid=$(echo $tmp | jq -r '.data.uid')
+memberid=$(echo $tmp | jq -r '.data.memberID')
+content=$(curl -sk http://ililil.cn:66/api/yy.php | awk -F "," '{print $2}')
+if [ $(expr length "$content") -lt 10 ]; then
+content=福田汽车veryveryverygood
+fi
+tzid=$(curl -sk -X POST -H "host: $url" -H "content-type: application/json; charset=utf-8" -H "app-token: $apptoken" -d '{"memberId":"'$code'","userId":"'$uid'","userType":"61","uid":"'$uid'","mobile":"'$phone'","tel":"'$phone'","phone":"'$phone'","brandName":"","seriesName":"","token":"'$token'","safeEnc":'$(($(date '+%s%3N')-20220202))',"businessId":1,"content":"'$content'","postType":1,"topicIdList":[186],"uploadFlag":3,"title":"","urlList":[]}' "https://$url/ehomes-new/ehomesCommunity/api/post/addJson2nd" | jq -r '.data.postId')
+curl -sk -X POST -H "host: $url" -H "content-type: application/json; charset=utf-8" -H "app-token: $apptoken" -d '{"memberId":"'$code'","userId":"'$uid'","userType":"61","uid":"'$uid'","mobile":"'$phone'","tel":"'$phone'","phone":"'$phone'","brandName":"","seriesName":"","token":"'$token'","safeEnc":'$(($(date '+%s%3N')-20220202))',"businessId":1}' "https://$url/ehomes-new/homeManager/api/bonus/signActivity2nd" >/dev/null
+curl -sk -X POST -H "Content-Type: application/json;charset=utf-8" -H "Host: $url" -d '{"safeEnc":'$(($(date '+%s%3N')-20220202))',"activity":"","tel":"'$phone'","id":"33","source":"APP","memberId":"'$code'"}' "https://$url/ehomes-new/homeManager/api/bonus/addIntegralForShare" >/dev/null
+gzid=$((RANDOM + 8010000))
+curl -sk -X POST -H "host: $url" -H "content-type: application/json; charset=utf-8" -H "app-token: $apptoken" -d '{"memberId":"'$code'","userId":"'$uid'","userType":"61","uid":"'$uid'","mobile":"'$phone'","tel":"'$phone'","phone":"'$phone'","brandName":"","seriesName":"","token":"'$token'","safeEnc":'$(($(date '+%s%3N')-20220202))',"businessId":1,"behavior":"1","memberIdeds":"'$gzid'","navyId":"null"}' "https://$url/ehomes-new/ehomesCommunity/api/post/follow2nd" >/dev/null
+curl -sk -X POST -H "host: $url" -H "content-type: application/json; charset=utf-8" -H "app-token: $apptoken" -d '{"memberId":"'$code'","userId":"'$uid'","userType":"61","uid":"'$uid'","mobile":"'$phone'","tel":"'$phone'","phone":"'$phone'","brandName":"","seriesName":"","token":"'$token'","safeEnc":'$(($(date '+%s%3N')-20220202))',"businessId":1,"behavior":"2","memberIdeds":"'$gzid'","navyId":"null"}' "https://$url/ehomes-new/ehomesCommunity/api/post/follow2nd" >/dev/null
+curl -sk -X POST -H "host: $url" -H "content-type: application/json; charset=utf-8" -H "app-token: $apptoken" -d '{"memberId":"'$memberid'","userId":"'$uid'","userType":"61","uid":"'$uid'","mobile":"'$phone'","tel":"'$phone'","phone":"'$phone'","brandName":"","seriesName":"","token":"'$token'","safeEnc":'$(($(date '+%s%3N')-20220202))',"businessId":1,"postId":'$tzid'}' "https://$url/ehomes-new/ehomesCommunity/api/mine/delete" >/dev/null
+echo "$phone任务运行成功，稍等一会儿积分到账"
+sleep $((RANDOM % 60))
+echo "福田e家帐号$phone目前积分$(curl -sk -X POST -H "host: $url" -H "content-type: application/json; charset=utf-8" -H "app-token: $apptoken" -d '{"memberId":"'$memberid'","userId":"'$uid'","userType":"61","uid":"'$uid'","mobile":"'$phone'","tel":"'$phone'","phone":"'$phone'","brandName":"","seriesName":"","token":"'$token'","safeEnc":'$(($(date '+%s%3N')-20220202))',"businessId":1}' "https://$url/ehomes-new/homeManager/api/Member/findMemberPointsInfo" | jq -r '.data.pointValue')"
+done
