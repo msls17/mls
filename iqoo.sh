4e911cd148c844cbb065abd14fed8efb29413911
msls17 <33090354+msls17@users.noreply.github.com>
2024-04-27 16:51:41 +0800

    Add files via upload

--- iqoo.sh
+++ iqoo.sh
@@ -5,6 +5,9 @@
 token=($(echo $iqoo | sed 's/&/ /g'))
 url=bbs-api.iqoo.com
 key=2618194b0ebb620055e19cf9811d3c13
+d(){
+echo "$tmp" | jq -r '.Meta.tips[].message'
+}
 p(){
 a=$(date '+%s')
 c=""
@@ -18,7 +21,7 @@ r=$(echo -n "GET&$t&$c&$l&appid=1002&timestamp=$a" | openssl dgst -sha256 -hmac 
 tmp=$(curl -sk -X GET -H "Host: $url" -H "authorization: ${token[$s]}" -H "sign: IQOO-HMAC-SHA256 appid=1002,timestamp=$a,signature=$r" -H "x-platform: mini" -H "content-type: application/json" "https://$url$t?$c")
 }
 for s in $(seq 0 1 $((${#token[@]}-1)));do
-echo "..................开始执行iqoo账号$s.................."
+echo "........开始执行iqoo账号$s........"
 t="/api/v3/sign"
 l='{"from":""}'
 p
@@ -26,12 +29,12 @@ if [ $(echo "$tmp" | jq -r '.Code') -ne 0 ]; then
 echo "ck可能失效，请重新抓包"
 curl -sk -X POST -H "Host: wxpusher.zjiecode.com" -H "Content-Type: application/json" -d '{"appToken":"'$apptoken'","content":"iqoo账号'$s'可能失效，请重新抓包","contentType":1,"topicIds":['$topicId'], "url":"https://wxpusher.zjiecode.com","verifyPay":false}' "https://wxpusher.zjiecode.com/api/send/message" | jq -r '.msg'
 else
-echo "$tmp" | jq -r '.Meta.tips[].message'
+d
 tmp=$(curl -sk http://ililil.cn:66/api/yy.php)
 t="/api/v3/thread.create"
 l='{"title":"'$(echo $tmp | awk -F "," '{print $1}')'","categoryId":27,"content":{"text":"<p>'$(echo $tmp | awk -F "," '{print $2}')'</p>"},"position":{},"price":0,"freeWords":0,"attachmentPrice":0,"draft":0,"anonymous":0,"topicId":"","source":"","videoId":""}'
 p
-echo "$tmp" | jq -r '.Meta.tips[].message'
+d
 t="/api/v3/thread.delete"
 l='{"threadId":'$(echo "$tmp" | jq -r '.Data.threadId')',"message":"1"}'
 p
@@ -44,17 +47,17 @@ for i in $(seq 2);do
 t="/api/v3/view.count"
 c='threadId='${tzid[$i]}'&type=0'
 g
-echo "$tmp" | jq -r '.Meta.tips[].message'
+d
 done
 for i in $(seq 4);do
 t="/api/v3/posts.update"
 l='{"id":'${tzid[$i]}',"postId":'${postid[$i]}',"data":{"attributes":{"isLiked":true}}}'
 p
-echo "$tmp" | jq -r '.Meta.tips[].message'
+d
 t="/api/v3/thread.share"
 l='{"threadId":"'${tzid[$i]}'"}'
 p
-echo "$tmp" | jq -r '.Meta.tips[].message'
+d
 done
 fi
 wait
