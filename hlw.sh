7e62debd0955b07ea25f39f9734faf6241d1c655
msls17 <33090354+msls17@users.noreply.github.com>
2024-05-23 03:10:06 +0800

    Add files via upload

--- hlw.sh
+++ hlw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # new Env('wx-葫芦娃茅台');
-#by-莫老师，版本1.1
+#by-莫老师，版本1.3
 #cron:5 10 * * *
 host=gw.huiqunchina.com
 wxappid=(wxded2e7e6d60ac09d@偲源惠购 wx61549642d715f361@贵旅优品 wx613ba8ea6a002aa8@空港乐购 wx936aa5357931e226@航旅黔购 wx624149b74233c99a@遵航出山 wx5508e31ffe9366b8@贵盐黔品 wx821fb4d8604ed4d6@乐旅商城 wxee0ce83ab4b26f9c@驿路黔寻)
@@ -15,7 +15,7 @@ echo "未成功获取到code，正尝试重新获取"
 getcode
 else
 echo "获取code失败，萝卜未启动"
-curl -sk -X POST -H "Host: wxpusher.zjiecode.com" -H "Content-Type: application/json" -d '{"appToken":"'$apptoken'","content":"获取code失败，请检查code服务器是否正常","contentType":1,"topicIds":['$topicId'], "url":"https://wxpusher.zjiecode.com","verifyPay":false}' "https://wxpusher.zjiecode.com/api/send/message" | sed 's/,/\n/g' | grep "msg" | awk -F ":" '{print $2}'
+curl -sk -X POST -H "Host: wxpusher.zjiecode.com" -H "Content-Type: application/json" -d '{"appToken":"'$apptoken'","content":"获取code失败，请检查code服务器是否正常","contentType":1,"topicIds":['$topicId'], "url":"https://wxpusher.zjiecode.com","verifyPay":false}' "https://wxpusher.zjiecode.com/api/send/message" | jq -r '.msg'
 exit
 fi
 fi
@@ -24,7 +24,7 @@ getsign() {
 time=$(date -u +"%a, %d %b %Y %H:%M:%S GMT")
 sign=$(echo "$(printf "$method\n$url\n\n$ak\n$time\n")" | openssl dgst -sha256 -hmac "$sk" -binary | openssl base64)
 digest=$(echo -n "$body" | openssl dgst -sha256 -hmac "$sk" -binary | base64)
-tmp=$(curl -sk -X POST -H "Host: $host" -H "X-HMAC-Date: $time" -H "dataType: json" -H "X-HMAC-DIGEST: $digest" -H "Content-Type: application/json" -H "channelid: $chanid" -H "X-HMAC-SIGNATURE: $sign" -H "X-HMAC-ALGORITHM: hmac-sha256" -H "Channel: miniapp" -H "X-HMAC-ACCESS-KEY: $ak" -H "X-access-token: ${token[$i]}" -d "$body" "https://$host$url")
+tmp=$(curl -sk -X POST -H "Host: $host" -H "X-HMAC-Date: $time" -H "dataType: json" -H "X-HMAC-DIGEST: $digest" -H "Content-Type: application/json" -H "channelid: $chanid" -H "X-HMAC-SIGNATURE: $sign" -H "X-HMAC-ALGORITHM: hmac-sha256" -H "Channel: miniapp" -H "X-HMAC-ACCESS-KEY: $ak" -H "X-access-token: $token" -d "$body" "https://$host$url")
 }
 for s in $(seq 0 1 $((${#wxappid[@]}-1)));do
 appid=$(echo ${wxappid[$s]} | awk -F "@" '{print $1}')
@@ -39,6 +39,7 @@ chanid=$(echo "$tmp" | jq -r '.data')
 url=/front-manager/api/customer/promotion/channelActivity
 body='{}'
 getsign
+activity=$(echo "$tmp" | jq -r '.data.id')
 edtime=$(date -d @$(echo "$tmp" | jq -r '.data.appointEndTime' | cut -c1-10) +%Y-%m-%d)
 cjtime=$(date -d @$(echo "$tmp" | jq -r '.data.drawTime' | cut -c1-10) +%Y-%m-%d)
 if [ "$cjtime" = "$today" ] || [ "$edtime" = "$today" ]; then
@@ -47,13 +48,8 @@ for i in $(seq 0 1 $((${#code[@]}-1)));do
 url=/front-manager/api/login/wxMiniLogin
 body='{"appId":"'$appid'","code":"'${code[$i]}'"}'
 getsign
-token[$i]=$(echo "$tmp" | jq -r '.data.token')
-done
-else
-echo "$name$today无活动"
-fi
+token=$(echo "$tmp" | jq -r '.data.token')
 if [ "$cjtime" = "$today" ]; then
-for i in $(seq 0 1 $((${#token[@]}-1)));do
 url=/front-manager/api/customer/promotion/queryLotteryRecord
 body='{"page":{"pageNo":1,"pageSize":20}}'
 getsign
@@ -61,17 +57,25 @@ zqsj=($(echo "$tmp" | jq -r '.data.list[].activityTime'))
 for t in "${zqsj[@]}";do
 if [ "$(date -d "@$t" '+%Y-%m-%d')" == "$today" ]; then
 echo "账号$i$name$today已中签"
-else
-echo "账号$i$name昨日未中签"
 fi
-done
 done
 fi
 if [ "$edtime" = "$today" ]; then
-activity=$(echo "$tmp" | jq -r '.data.appointEndTime')
+url=/front-manager/api/customer/promotion/checkMnsAuth
+body='"channelId":'$chanid'}'
+getsign
+if [[ "$tmp" == *"验证码"* ]]; then
+curl -sk -X POST -H "Host: wxpusher.zjiecode.com" -H "Content-Type: application/json" -d '{"appToken":"'$apptoken'","content":"'$(echo "$tmp" | jq -r '.message')'","contentType":1,"topicIds":['$topicId'], "url":"https://wxpusher.zjiecode.com","verifyPay":false}' "https://wxpusher.zjiecode.com/api/send/message" | jq -r '.msg'
+else
 url=/front-manager/api/customer/promotion/appoint
 body='{"activityId":'$activity',"channelId":'$chanid'}'
 getsign
 echo "账号$i$name$(echo "$tmp" | jq -r '.message')"
 fi
+fi
+done
+else
+echo "$name$today无活动"
+fi
+unset token
 done
