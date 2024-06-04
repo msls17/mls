18fe043ca33f5973a320dadd1a04642933ca7368
msls17 <33090354+msls17@users.noreply.github.com>
2024-06-04 08:49:07 +0800

    Add files via upload

--- zgrb.sh
+++ zgrb.sh
@@ -21,50 +21,6 @@ device=$(echo "${zh[$s]}" | awk -F "@" '{print $2}')
 ck=$(curl -sik -X POST -H "Content-Type: application/json; charset=UTF-8" -H "Host: $url" -d '{"body":{"signInType":"0","thirdPartyId":"'$openid'"},"head":{"accessToken":"","adCode":"350100","appInfo":{"appBuild":"261","appVersion":"6.22.2"},"deviceInfo":{"deviceId":"'$device'","deviceModel":"23049RAD8C","osType":"android","osVersion":"13","romType":"2","romVersion":"0"},"tags":{"tags":[],"tagsLogin":[]},"token":"","userId":""},"uuid":"'$uuidgen'"}' "https://$url/G-BASE/a/user/login/thirdPartyLogin/v1" | sed 's/\n//g' | sed 's/ //g' | grep "Authorization" | awk -F ":" '{print $2}' | awk -F ";" '{print $1}')
 rm -rf zgrb.log
 echo "........中国人保账号$s........"
-#
-lj=$(curl -sik -X GET -H "Host: $url" -H "Cookie: w_a_t=$ck" "https://$url/G-OPEN/oauth2/authorize/v1?client_id=5TuIXelNdOotDUWXRhPid1NZ&scope=auth_user&response_type=code&state=eyJzb3VyY2VfZW50cnkiOiIiLCJhY3Rpdml0eV9zb3VyY2UiOiIiLCJhY3Rpdml0eV9jb2RlIjoiQXBwQ3VzdG9tZXIyNCIsInBsYXRmb3JtIjoiNyJ9&redirect_uri=https://m.picclife.cn/cust/api/activity-access-code/get-token" | grep "Location" | awk -F " " '{print $2}')
-lj2=$(curl -sik "$lj" | grep "location" | awk -F " " '{print $2}')
-token="$(curl -sik "$lj2" | grep "CUSTOMER2024" | awk -F "CUSTOMER2024=" '{print $2}' | awk -F ";" '{print $1}')"
-for i in 1 2;do
-getsign
-curl -sk -X POST -H "Host: $url3" -H "Content-Type: application/json" -H "sign: $sign" -H "Cookie: PICC_APP_CUSTOMER2024=$token" -d '{"timestamp":'$time',"id":'$i'}' "https://$url3/api/task/complete" | jq -r '.msg'
-sleep 1
-getsign
-curl -sk -X POST -H "Host: $url3" -H "Content-Type: application/json" -H "sign: $sign" -H "Cookie: PICC_APP_CUSTOMER2024=$token" -d '{"timestamp":'$time',"taskId":'$i'}' "https://$url3/api/task/claimReward" | jq -r '.msg'
-sleep 1
-done
-for i in 6 6 6;do
-hqsign
-tmp=$(curl -sk -X POST -H "Host: $url3" -H "Content-Type: application/json" -H "sign: $sign" -H "Cookie: PICC_APP_CUSTOMER2024=$token" -d '{"timestamp":'$time'}' "https://$url3/api/task/view-$i")
-key=$(echo -e "$tmp" | grep -oP '(?<=key=)[^&"]+')
-sleep 15
-curl -sk -X GET -H "Host: piccapp-api.picclife.cn" -H "combCode: GAESCQYLBX" -H "utmSource: 218" -H "platform: 7" "https://piccapp-api.picclife.cn/gateway-api/ebs-api/api/maxrocky/callBackMaxrocky?key=$key" | jq -r '.msg'
-sleep 1
-getsign
-curl -sk -X POST -H "Host: $url3" -H "Content-Type: application/json" -H "sign: $sign" -H "Cookie: PICC_APP_CUSTOMER2024=$token" -d '{"timestamp":'$time',"taskId":'$i'}' "https://$url3/api/task/claimReward" | jq -r '.msg'
-done
-hqsign
-tmp=$(curl -sk -X POST -H "Host: $url3" -H "Content-Type: application/json" -H "sign: $sign" -H "Cookie: PICC_APP_CUSTOMER2024=$token" -d '{"timestamp":'$time'}' "https://$url3/api/config")
-yxcs=$(echo "$tmp" | jq -r '.data.user.playGameNum')
-cjcs=$(echo "$tmp" | jq -r '.data.user.lotteryNum.a')
-sleep 1
-for i in $(seq $yxcs );do
-cj=99
-hqsign
-tmp=$(curl -sik -X POST -H "Host: $url3" -H "Content-Type: application/json" -H "sign: $sign" -H "Cookie: PICC_APP_CUSTOMER2024=$token" -d '{"timestamp":'$time'}' "https://$url3/api/game/config")
-game=$(echo "$tmp" | grep -oP '"gameId"\s*:\s*"\K[^"]+')
-app=$(echo "$tmp" | grep -oP 'X-App-Game:\s*\K[^[:space:]]+')
-sleep 60
-getsign
-curl -sk -X POST -H "Host: $url3" -H "X-App-Game: $app" -H "Content-Type: application/json" -H "sign: $sign" -H "Cookie: PICC_APP_CUSTOMER2024=$token" -d '{"timestamp":'$time',"gameId":'$game',"score": '$cj'}' "https://$url3/api/game/end"
-sleep 1
-done
-for i in $(seq $cjcs );do
-hqsign
-curl -sk -X POST -H "Host: $url3" -H "Content-Type: application/json" -H "sign: $sign" -H "Cookie: PICC_APP_CUSTOMER2024=$token" -d '{"timestamp":'$time'}' "https://$url3/api/lottery/draw.a" | jq -r '.msg'
-sleep 1
-done
-#
 lj=$(curl -sik -X GET -H "Host: $url" -H "Cookie: w_a_t=$ck" "https://$url/G-OPEN/oauth2/authorize/v1?client_id=EC8XhCVQNN5dha8huaRZEC1v&scope=auth_user&response_type=code&redirect_uri=https%3A%2F%2F$url2%2Fdop%2Fscoremall%2Fuser%2FappLoginCallback%3FafterLoginRedirectUrl%3Dhttps%2525253A%2525252F%2525252F$url2%2525252Fdop%2525252Fscoremall%2525252Fmall%2525252F%25252523%2525252FdailyAttendance%2525253Fapply%2525253Dapp" | grep "Location" | awk -F " " '{print $2}')
 token=$(curl -sik -X GET -H "Host: $url2" "$lj" | grep "app-token" | awk -F "app-token=" '{print $2}' | awk -F ";" '{print $1}')
 if [ -z "$token" ]; then
