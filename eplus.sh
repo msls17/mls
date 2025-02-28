7bfd2782bae530fbc7b804f539e30e12fc3ced34
msls17 <33090354+msls17@users.noreply.github.com>
2025-02-28 10:13:09 +0800

    Add files via upload

--- eplus.sh
+++ eplus.sh
@@ -21,14 +21,12 @@ for s in $(seq 0 1 0)
 do
 token=$(curl -sk -X POST -H "Host: $url" -H "content-type: application/x-www-form-urlencoded;charset=UTF-8" -d "code=${code[$s]}&appId=wxe5688eab75967e0a&scope=snsapi_base&loginParams=&actId=&unionId=&elifeParam=null" "https://$url/wx/user/getTokenByParams.do" | jq -r '.body.token')
 if [ "$(date +%u)" -eq 4 ]; then
-id=($(curl -sk -X POST -H "Host: $url" -H "x-token: $token" -H "content-type: application/x-www-form-urlencoded;charset=UTF-8" -d "" "https://$url/activity/icbc/tuesday/v1/checkEquity.do" | jq '.body.product[].type'))
-for i in $(seq 0 1 $((${#id[@]}-1)))
-do
-echo "账号$s兑换奖品：$(curl -sk -X POST -H "Host: $url" -H "x-token: $token" -H "content-type: application/x-www-form-urlencoded;charset=UTF-8" -d "" "https://$url/activity/icbc/tuesday/v1/receiveEquity.do?type=${id[$i]}&mobile=$eplus" | jq -r '.msg')"
+for i in 2000;do
+echo "账号$s兑换奖品：$(curl -sk -X POST -H "Host: $url" -H "x-token: $token" -H "content-type: application/x-www-form-urlencoded;charset=UTF-8" -d "" "https://$url/activity/icbc/tuesday/v1/receiveEquity.do?type=$i&mobile=$eplus" | jq -r '.msg')"
 done
 elif [ "$(date +%u)" -eq 5 ]; then
 for i in 2006 2007 2002;do
-echo "账号$s兑换奖品：$(curl -sk -X POST -H "Host: $url" -H "x-token: $token" -H "content-type: application/x-www-form-urlencoded;charset=UTF-8" -d "" "https://$url/activity/icbc/friday/v1/receiveEquity.do?type=${id[$i]}&mobile=$eplus" | jq -r '.msg')"
+echo "账号$s兑换奖品：$(curl -sk -X POST -H "Host: $url" -H "x-token: $token" -H "content-type: application/x-www-form-urlencoded;charset=UTF-8" -d "" "https://$url/activity/icbc/friday/v1/receiveEquity.do?type=$i&mobile=$eplus" | jq -r '.msg')"
 done
 else
 echo "还没到活动日哦"
