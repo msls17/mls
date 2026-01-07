d81db9aa3d300364aabb64ce40c39c8ef4bde978
msls17 <33090354+msls17@users.noreply.github.com>
2026-01-07 12:21:47 +0800

    Add files via upload

--- pinzan.sh
+++ pinzan.sh
@@ -1,9 +1,9 @@
 # new Env('品赞代理');
 #by-莫老师，版本1.0
 #青龙创建变量pinz，值为手机号@登录密码@aes密钥@套餐编号@套餐密钥
-#例如13888888888@ABC12345@9c13sltjjjd5gk0a@20250101797178617771@46isnsn9jdn4uhx
+#例如13888888888@ABC12345@9c13sltjjjd5gk0a@20250101797178617771@46isnsn9jdn4uhx@71KUVS8SH7K
 #cron:0 8 * * 1
-IFS='@' read -r pzphone pass aeskey no key <<< "$pinz"
+IFS='@' read -r pzphone pass aeskey no key userid <<< "$pinz"
 encoded=$(echo -n "${pzphone}QWERIPZAN1290QWER${pass}" | base64 | tr -d '\n')
 t=$(head -c 400 /dev/urandom | xxd -p -l 200 | tr -d '\n' | head -c 400)
 account=$(echo "${t:0:100}${encoded:0:8}${t:100:100}${encoded:8:12}${t:200:100}${encoded:20}${t:300:100}")
